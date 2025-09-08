d84694729f9631dc085a12a07e1e8c3a2a1c591e
Rashi Kushwaha <167156022+rashyy17@users.noreply.github.com>
2025-09-09 00:31:08 +0530

    Implement palindrome check in Solution class

---
+++ 125.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i=0,j=s.length()-1;
+        while(i<j){
+            if(!Character.isLetterOrDigit(s.charAt(i))){
+                i++;a
+                continue;
+            }
+            if(!Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+                continue;
+            }
+            if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))){
+                return false;
+            }
+            else{
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+}
