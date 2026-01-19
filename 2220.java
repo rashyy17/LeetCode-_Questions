282831f582131237984e44194ff4e10592fa61c9
Rashi Kushwaha <167156022+rashyy17@users.noreply.github.com>
2026-01-20 00:09:02 +0530

    Implement minBitFlips method in Solution class

---
+++ 2220.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int xor= start^goal;
+        int c=0;
+        while(xor>0){
+            if((xor&1)==1) c++;
+            xor=xor>>1;
+        }
+        return c;
+    }
+}
