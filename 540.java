61543a1e36c387b706b5134633aacf94217fa1de
Rashi Kushwaha <167156022+rashyy17@users.noreply.github.com>
2026-02-26 11:26:41 +0530

    Refactor singleNonDuplicate method using binary search

--- 540.java
+++ 540.java
@@ -1,31 +1,21 @@
+//using binary searchhh
+
+
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int ans=0;
-        if(nums.length==1){
-            return nums[0];
-        }
-        else if(nums.length==2 && nums[0]==nums[1]){
-            return nums[0];
-        }
-        else if(nums.length==3){
-            if(nums[0]==nums[1]){
-                return nums[2];
+        int s=0,e=nums.length-1;
+        while(s<e){
+            int m=s+(e-s)/2;
+            if(m%2==1){
+                m--;
             }
-            else if(nums[1]==nums[2]){
-                return nums[0];
+            if(nums[m]!=nums[m+1]){
+                e=m;
+            }
+            else{
+                s=m+2;
             }
         }
-        for(int i=1;i<nums.length-1;i++){
-            if(nums[i-1]==nums[i] || nums[i]==nums[i+1]){
-                if(nums[i+1]!=nums[i] && i+1==nums.length-1){
-                    return nums[i+1];
-                }
-                else if(nums[i-1]!=nums[i] && i-1==0){
-                    return nums[0];
-                }
-            }
-            else ans=nums[i];
-        }
-        return ans;
+        return nums[s];
     }
 }
