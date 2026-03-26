e1e47ef91c49221e2e474f9f4d90aa6b9a378ec9
Rashi Kushwaha <167156022+rashyy17@users.noreply.github.com>
2026-03-26 22:50:29 +0530

    Create 129.java

---
+++ 129.java
@@ -0,0 +1,15 @@
+class Main {
+    static int sumNumbers(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    static int dfs(TreeNode node, int current) {
+        if (node == null) return 0;
+
+        current = current * 10 + node.val;
+
+        if (node.left == null && node.right == null) return current;
+
+        return dfs(node.left, current) + dfs(node.right, current);
+    }
+}
