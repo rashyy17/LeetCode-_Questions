067dbffb414d56bf81d01926e81f2fa786ddef1f
Rashi Kushwaha <167156022+rashyy17@users.noreply.github.com>
2026-03-29 12:24:07 +0530

    Update 61.java

--- 61.java
+++ 61.java
@@ -1,30 +1,35 @@
-//"Make it circular → then break"
+//Make it circular by making end.next=head 
+//then break (going to kth position and making the node after k null)
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if (head == null || head.next == null) return head;
-
-        ListNode temp = head;
-        int len = 1;
-
-        while (temp.next != null) {
-            temp = temp.next;
+        if (head==null) return head;
+        ListNode l;
+        int len=1;
+        ListNode dummy=head;
+        while(dummy.next!=null){
             len++;
+            dummy=dummy.next;
         }
-
-        temp.next = head;
-
-        k = k % len;
-        int steps = len - k;
-
-        ListNode newTail = head;
-        for (int i = 1; i < steps; i++) {
-            newTail = newTail.next;
+        dummy.next=head;
+        k=k%len;
+        int steps=len-k;
+        ListNode tailEnd=head;
+        for(int i=1;i<steps;i++){
+            tailEnd=tailEnd.next;
         }
-
-        ListNode newHead = newTail.next;
-        newTail.next = null;
-
-        return newHead;
+        ListNode ans=tailEnd.next;
+        tailEnd.next=null;
+        return ans;
     }
 }
