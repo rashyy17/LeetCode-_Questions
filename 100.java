c812228c4a81183f08da660cd04d761640bd4850
Rashi Kushwaha <167156022+rashyy17@users.noreply.github.com>
2026-04-13 23:55:29 +0530

    Update 100.java

--- 100.java
+++ 100.java
@@ -1,3 +1,7 @@
+// if both heads null, return true.
+// if only one of the heads is null, return false.
+// if values are not same, return false. 
+// run again on left and right branches
 class Solution {
     boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) return true;
