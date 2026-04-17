8b8fb3f7d4273ae158060126c73de5383ec15ad4
Rashi Kushwaha <167156022+rashyy17@users.noreply.github.com>
2026-04-18 02:03:57 +0530

    Update 99.java

--- 99.java
+++ 99.java
@@ -13,6 +13,34 @@
  *     }
  * }
  */
+Only Two nodes have to switched to make BST correct.
+ 
+    So, two possible cases: i) Both wrong values are next to each other.   ex. 1,2,4,3,5,6 (3 and 4)
+                            ii) Both values are not next to each other.( far apart)   ex. 1,6,3,4,5,2 (6 and 2)
+
+    Common part of solution (inorder traversal gives sorted array intuition):
+        have 3 nodes: first, second and last. 
+        prev will store last node and will help identify wrong node (prev.val<root.val)
+        
+                                                                    
+    In case i) so both(prev and root) will be stored in first and second respectively. last will obv remain null.
+    In case ii)initially,  both(prev and root) will be stored in first and second respectively. when next element found wrong, 
+    that root is stored in last(variable).
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     TreeNode f,s,l;
     TreeNode prev=new TreeNode(Integer.MIN_VALUE);
@@ -39,8 +67,7 @@ class Solution {
             s=root;
            }
             else{
-                s=root;
-            
+                l=root;
             }
         }
         prev=root;
