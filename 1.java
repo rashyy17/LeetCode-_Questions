b6126a911d9f1e2000f7ad06c2c504bfa929bc36
Rashi Kushwaha <167156022+rashyy17@users.noreply.github.com>
2026-07-04 23:42:59 +0530

    Revise comments in 1.java

--- 1.java
+++ 1.java
@@ -1,5 +1,5 @@
 //revised
-//3
+//
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] nums2=new int[2];
@@ -14,5 +14,5 @@ class Solution {
         return nums2;
     }
 }
-//5
+//6
 
